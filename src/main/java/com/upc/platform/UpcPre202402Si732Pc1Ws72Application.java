3c22833672e436c6fd7c4f9a981d701c7156aa14
Salim Ramirez <salimramirezm@gmail.com>
2025-09-24 22:44:25 -0500

    chore: initial commit

---
+++ src/main/java/com/upc/platform/UpcPre202402Si732Pc1Ws72Application.java
@@ -0,0 +1,13 @@
+package com.upc.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpcPre202402Si732Pc1Ws72Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UpcPre202402Si732Pc1Ws72Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/upc/platform/UpcPre202402Si732Pc1Ws72ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upc.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpcPre202402Si732Pc1Ws72ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
