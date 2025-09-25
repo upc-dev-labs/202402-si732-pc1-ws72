ba8c61bfdb91040ddf5f306c2195d311eb640d02
Salim Ramirez <salimramirezm@gmail.com>
2025-09-25 01:09:12 -0500

    feat: add JPA annotations and constructor to Dueno class

--- src/main/java/com/upc/platform/model/Dueno.java
+++ src/main/java/com/upc/platform/model/Dueno.java
@@ -1,5 +1,6 @@
 package com.upc.platform.model;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,15 +8,22 @@ import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
 
+@Entity
+@Table(name = "duenos")
 @Getter
 @Setter
 public class Dueno {
     // Getters y setters
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
     private String nombre;
     private String apellido;
     private String telefono;
     private String direccion;
+
+    protected Dueno() {} // constructor JPA
 
     public Dueno(String nombre, String apellido, String telefono, String direccion) {
         this.nombre = nombre;
